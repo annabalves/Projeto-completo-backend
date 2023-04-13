d306dfc823342d9e006d3cd7e5310de50b4c3ad1
annabalves <annaalves197@gmail.com>
2023-04-12 21:52:56 -0300

    first commit

---
+++ pages/filmes/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import Pagina from '../../components/Pagina'
+import apiFilmes from '../../services/apiFilmes'
+
+const index = () => {
+    const[filmes, setFilmes] = useState([])
+
+    useEffect(()=>{
+        apiFilmes.get('/movie/popular').then(resultado =>{
+            setFilmes(resultado.data.results)
+        })
+    }, [])
+  return (
+    <Pagina titulo='Filmes'>
+        <div>Filmes</div>
+
+        {filmes.map(item => (
+            <p>{item.title}</p>
+        ))}
+    </Pagina>
+  )
+}
+
+export default index
---
+++ services/apiFilmes.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+const apiFilmes = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzRlZGViYjgxZGFkZjNmNzg3MDQzOTZjZGUzYTc5OSIsInN1YiI6IjY0MzVlOGE0OWFjNTM1MDA5ZDM3YzgwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.89-v_CAVYpp45NlQLellL6cgSA5oB1dkfZa7GxKSzx4'
+    }
+})
+
+export default apiFilmes
